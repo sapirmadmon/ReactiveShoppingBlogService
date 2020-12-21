f9a7db573119dd58403d42da292f1a1577ff87af
sapir <sapir.madmon@s.afeka.ac.il>
2020-12-21 12:12:30 +0200

    first commit

---
+++ src/main/java/com/example/demo/ShoppingBlogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingBlogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingBlogServiceApplication.class, args);
+	}
+
+}
